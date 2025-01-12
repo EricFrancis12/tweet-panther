b5491d63c186281f7fb1a2a0a63cd46aacb21b3f
Eric <ericfrancis1@proton.me>
2025-01-12 17:23:08 -0600

    fix test delims

--- twitter_test.go
+++ twitter_test.go
@@ -68,36 +68,36 @@ func TestPublishTweetOpts(t *testing.T) {
 			newPublishTweetOptsTest("some text", "some text", false),
 
 			// Non-existing fields
-			newPublishTweetOptsTest("{{ stuff }}", "", true),
-			newPublishTweetOptsTest("{{ data.stuff }}", "", true),
+			newPublishTweetOptsTest("{*{ stuff }*}", "", true),
+			newPublishTweetOptsTest("{*{ data.stuff }*}", "", true),
 
 			// Post fields
-			newPublishTweetOptsTest("{{ data.post.title }}", "My Awesome Title", false),
-			newPublishTweetOptsTest("{{ data.post.timestamp }}", "12345678", false),
-			newPublishTweetOptsTest("{{ data.post.foo }}", "true", false),
-			newPublishTweetOptsTest("{{ data.post.bar }}", "false", false),
-			newPublishTweetOptsTest("{{ data.post.tags }}", `["Awesome","Cool"]`, false),
+			newPublishTweetOptsTest("{*{ data.post.title }*}", "My Awesome Title", false),
+			newPublishTweetOptsTest("{*{ data.post.timestamp }*}", "12345678", false),
+			newPublishTweetOptsTest("{*{ data.post.foo }*}", "true", false),
+			newPublishTweetOptsTest("{*{ data.post.bar }*}", "false", false),
+			newPublishTweetOptsTest("{*{ data.post.tags }*}", `["Awesome","Cool"]`, false),
 
 			// Nested object
-			newPublishTweetOptsTest("{{ data.post.stats.status }}", "published", false),
-			newPublishTweetOptsTest("{{ data.post.stats.traffic }}", "87654321", false),
-			newPublishTweetOptsTest("{{ data.post.stats.trending }}", "false", false),
-			newPublishTweetOptsTest("{{ data.post.stats.hot }}", "true", false),
-			newPublishTweetOptsTest("{{ data.post.stats.more }}", `{"some":"data"}`, false),
-			newPublishTweetOptsTest("{{ data.post.stats.more.some }}", "data", false),
-			newPublishTweetOptsTest("{{ data.post.stats.items }}", `["foo","bar","baz"]`, false),
+			newPublishTweetOptsTest("{*{ data.post.stats.status }*}", "published", false),
+			newPublishTweetOptsTest("{*{ data.post.stats.traffic }*}", "87654321", false),
+			newPublishTweetOptsTest("{*{ data.post.stats.trending }*}", "false", false),
+			newPublishTweetOptsTest("{*{ data.post.stats.hot }*}", "true", false),
+			newPublishTweetOptsTest("{*{ data.post.stats.more }*}", `{"some":"data"}`, false),
+			newPublishTweetOptsTest("{*{ data.post.stats.more.some }*}", "data", false),
+			newPublishTweetOptsTest("{*{ data.post.stats.items }*}", `["foo","bar","baz"]`, false),
 
 			// Indexing inside of array (TODO: not yet implimented)
-			newPublishTweetOptsTest("{{ data.post.stats.items[0] }}", "", true),
-			newPublishTweetOptsTest("{{ data.post.stats.items[1] }}", "", true),
-			newPublishTweetOptsTest("{{ data.post.stats.items[2] }}", "", true),
+			newPublishTweetOptsTest("{*{ data.post.stats.items[0] }*}", "", true),
+			newPublishTweetOptsTest("{*{ data.post.stats.items[1] }*}", "", true),
+			newPublishTweetOptsTest("{*{ data.post.stats.items[2] }*}", "", true),
 
 			// Objects inside of array
-			newPublishTweetOptsTest("{{ data.post.authors }}", `[{"name":"Jim"},{"name":"Bob"}]`, false),
+			newPublishTweetOptsTest("{*{ data.post.authors }*}", `[{"name":"Jim"},{"name":"Bob"}]`, false),
 
 			// Indexing objects inside of array (TODO: not yet implimented)
-			newPublishTweetOptsTest("{{ data.post.authors[0] }}", "", true),
-			newPublishTweetOptsTest("{{ data.post.authors[1] }}", "", true),
+			newPublishTweetOptsTest("{*{ data.post.authors[0] }*}", "", true),
+			newPublishTweetOptsTest("{*{ data.post.authors[1] }*}", "", true),
 		}
 
 		for _, test := range tests {
@@ -129,11 +129,11 @@ func TestPublishTweetOpts(t *testing.T) {
 
 		tests := []JsonFmtsTest{
 			{
-				text:     "The article title is {{ data.post.title }}.",
+				text:     "The article title is {*{ data.post.title }*}.",
 				expected: []string{"data.post.title"},
 			},
 			{
-				text:     "The article title is {{ data.post.title }}, and the timestamp is {{ data.post.timestamp }}.",
+				text:     "The article title is {*{ data.post.title }*}, and the timestamp is {*{ data.post.timestamp }*}.",
 				expected: []string{"data.post.title", "data.post.timestamp"},
 			},
 		}
